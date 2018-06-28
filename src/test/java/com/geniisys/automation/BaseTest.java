8c68cfea2f0baa4b30713140b432b3cd0f68ac40
CPI <CPI@CPI-PC>
2018-06-28 16:01:16 +0800

    fixed merge error

--- src/test/java/com/geniisys/automation/BaseTest.java
+++ src/test/java/com/geniisys/automation/BaseTest.java
@@ -1,5 +1,11 @@
 package com.geniisys.automation;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -9,26 +15,37 @@ import com.geniisys.automation.main.pages.LogInPage;
 public abstract class BaseTest {
 
 	protected BrowserDriver driver;
-//	private final String URL = "http://192.10.10.240:9090/Geniisys/";
-	private final String URL = "http://192.10.10.216:8080/Geniisys/";
-	private final String USERNAME = "CPI";
-	private final String PASSWORD = "CPI12345!";
+	private static final Logger LOGGER = LogManager.getLogger(BaseTest.class.getName());
+	
+	private String url;
+	private String username;
+	private String password;
 
 	@BeforeTest
 	public void setUp() {
+		Properties prop  = new Properties();
+
+		try {
+			prop.load(new FileInputStream("C:/SELENIUM-AUTOMATION/CONFIG/geniisys.properties"));
+		} catch (IOException e) {
+			LOGGER.error(e);
+		}
+		
+		url = prop.getProperty("url", "http://192.10.10.110:10/Geniisys");
+		username = prop.getProperty("username", "CPI");
+		password = prop.getProperty("password", "CPI12345!");
+
 		setDriver(new BrowserDriver("FIREFOX"));
 		driver.manage().window().maximize();
-		driver.get(URL);
+		driver.get(url);
 
 		LogInPage loginPage = new LogInPage(driver);
-		loginPage.logInAs(USERNAME, PASSWORD);
+		loginPage.logInAs(username, password);
 	}
 
 	@AfterTest
 	public void breakDown() {
-		if (null != driver) {
-			driver.quit();
-		}
+		driver.close();
 	}
 
 	public BrowserDriver getDriver() {
